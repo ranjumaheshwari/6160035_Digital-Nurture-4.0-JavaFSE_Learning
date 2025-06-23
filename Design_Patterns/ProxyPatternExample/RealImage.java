0f4e0942f73426ef04eb7a307f9613cc11e81e2e
ranjumaheshwari <maheshwarimalleshappa@gmail.com>
2025-06-23 20:34:35 +0530

    Add Proxy Pattern project for lazy image loading and caching

---
+++ Design_Patterns/ProxyPatternExample/RealImage.java
@@ -0,0 +1,17 @@
+public class RealImage implements Image {
+    private String filename;
+
+    public RealImage(String filename) {
+        this.filename = filename;
+        loadFromRemoteServer();
+    }
+
+    private void loadFromRemoteServer() {
+        System.out.println("Loading " + filename + " from remote server...");
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + filename);
+    }
+}
