42ea712c994b81a7995961759ea11e53990fe826
ranjumaheshwari <maheshwarimalleshappa@gmail.com>
2025-07-25 20:13:17 +0530

    Exercise-3 updated

---
+++ WEEK-6/Exercise-3/scorecalculatorapp/src/App.js
@@ -0,0 +1,16 @@
+import {CalculateScore} from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore
+        Name="Maheshwari M"
+        School="PES College of Engineering"
+        total={284}
+        goal={300}
+      />
+    </div>
+  );
+}
+
+export default App;
